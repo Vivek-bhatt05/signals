c71e0e41e7b4493f275c3f265939476bbf410b30
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2023-08-16 16:47:45 +0530

    dd

--- my-signals/src/App.js
+++ my-signals/src/App.js
@@ -1,27 +1,17 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import {Route, Routes } from "react-router-dom";
+import Join from "./component/Join/Join";
+import Chat from "./component/Chat/Chat"
+
+
+
 
 function App() {
-
-  const [count,setCount] = useState(0)
-
-  useEffect(()=>{
-    console.log('useEffect')
-  },[count])
-
-  const increment = () => {
-    setCount((prev)=> prev+1)
-  }
-  const decreament = () => {
-    setCount((prev)=> prev-1)
-  }
-
   return (
-    <div className="App">
-      <p>Count: {count}</p>
-      <button onClick={increment}>Add</button>
-      <button onClick={decreament}>Minus</button>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Join />} />
+      <Route path="/chat" element={<Chat />}/>
+    </Routes>
   );
 }
 
